8ba972fb66f6d671113c2864aa9b6d6c078314a6
Karolina Karmazaitė <karolinakarmazaite@gmail.com>
2022-04-26 10:22:20 +0300

    Initial commit

---
+++ components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <div id="logo"><i class="fa-solid fa-rocket-launch"></i> <strong>Space</strong> News</div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "AppHeader"
+
+}
+</script>
+
+<style>
+    header{
+        padding: 20px 0;
+        margin-bottom: 40px;
+        border-bottom: 1px solid var(--grey);
+    }
+    #logo{
+        font-size: 25px;
+    }
+    #logo strong{
+        color: var(--purple);
+    }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <div id="content-background">
+            <div id="content-container">
+                <AppHeader />
+                <Nuxt />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import AppHeader from "../components/AppHeader"
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    border: none;
+    outline: none;
+    box-sizing: border-box;
+}
+:root {
+    --black: rgb(67, 67, 67);
+    --grey: rgb(178, 178, 178);
+    --purple: #b15bb1;
+    --white: #f8f7f8;
+  }
+body {
+    font-family: 'Roboto', sans-serif;
+    font-size: 18px;
+    color: var(--black);
+
+    background-image: url("../static/BacgroundImage.png");
+    background-repeat: no-repeat, repeat;
+    background-size: cover; 
+    background-color: var(--purple);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height:100vh;
+   }
+   #content-background{
+    background-color: var(--white);
+    width: 80vw;
+    border-radius: 5px;
+    -webkit-box-shadow: 0px 0px 50px 3px rgba(0,0,0,0.6); 
+    box-shadow: 0px 0px 50px 3px rgba(0,0,0,0.6);
+}
+#content-background{
+    background-color: var(--white);
+    width: 80vw;
+    border-radius: 5px;
+    -webkit-box-shadow: 0px 0px 50px 3px rgba(0,0,0,0.6); 
+    box-shadow: 0px 0px 50px 3px rgba(0,0,0,0.6);
+}
+#content-container{
+    padding: 30px;
+}
+</style>
